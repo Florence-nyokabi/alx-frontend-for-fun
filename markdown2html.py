00b6c1c21d81bea629852306d5dbc0f3ea830e2d
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-01-15 12:00:40 +0300

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,148 +1,96 @@
 #!/usr/bin/python3
 
-'''
-Converts Markdown to HTML
-'''
+"""
+Markdown script using python.
+"""
+import sys
+import os.path
+import re
+import hashlib
 
-from sys import argv
-from sys import stderr
-from os import path
-
-
-def parse_headings(headings_list: list):
-    '''
-    Parses markdown headings and converts them to HTML heading tags
-    '''
-    html_headings = []
-
-    # If hash '#' is found, count number of hashes
-    for headings in headings_list:
-        if len(headings) > 0 and headings[0] == '#':
-            number_of_hashes = 0
-            while number_of_hashes < len(
-                    headings) and headings[number_of_hashes] == '#':
-                number_of_hashes += 1
-            # If more than 6 hashes, default to h6
-            if number_of_hashes > 6:
-                number_of_hashes = 6
-            html_tag = 'h' + str(number_of_hashes)
-            headings = headings.strip('#')
-            # Do not create a space between tag and text
-            headings = headings.strip(' ')
-            headings = '<' + html_tag + '>' + headings + '</' + html_tag + '>'
-        html_headings.append(headings)
-    return html_headings
-
-
-def parse_ordered_list(list_item: list):
-    '''
-    Parses markdown orderedlists and converts them to HTML heading tags
-    '''
-    html_list = []
-    is_ul = False
-    # If '-' is found
-    for uls in list_item:
-        if len(uls) > 0 and uls[0] == '-':
-            if is_ul:
-                # Create li tags and blank space on left so text comes after
-                # tag
-                html_list.append('<li>' + uls[1:].lstrip(' ') + '</li>')
-            # Create ul with more than one list item
-            else:
-                is_ul = True
-                html_list.append('<ul>')
-                html_list.append('<li>' + uls[1:].lstrip(' ') + '</li>')
-        # Find end of list items and close tags
-        elif (len(uls) == 0 or uls[0] != '-') and is_ul:
-            is_ul = False
-            html_list.append('</ul>')
-            html_list.append(uls)
-        else:
-            html_list.append(uls)
-    return html_list
-
-
-def parse_unordered_list(list_item: list):
-    '''
-    Parses markdown unordered lists and converts them to HTML heading tags
-    '''
-    html_list = []
-    is_ol = False
-    for ols in list_item:
-        if len(ols) > 0 and ols[0] == '*':
-            if is_ol:
-                html_list.append('<li>' + ols[1:].lstrip(' ') + '</li>')
-            else:
-                is_ol = True
-                html_list.append('<ol>')
-                html_list.append('<li>' + ols[1:].lstrip(' ') + '</li>')
-        # Find end of list items and close tags
-        elif (len(ols) == 0 or ols[0] != '*') and is_ol:
-            is_ol = False
-            html_list.append('</ol>')
-            html_list.append(ols)
-        else:
-            html_list.append(ols)
-    return html_list
-
-
-def file_read_write():
-    '''
-    Parses markdown file, makes appropriate method call
-    and creates HTML file with correct tags
-    '''
-
-    '''
-    Error messages
-    '''
-    if len(argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=stderr)
-        exit(1)
-    if not path.exists(argv[1]):
-        print('Missing {}'.format(argv[1]), file=stderr)
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
         exit(1)
 
-    markdown_file = argv[1]
-    output_file = argv[2]
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
 
-    '''
-    List of markdown file elements
-    '''
-    md_parser_list = []
+    with open(sys.argv[1]) as read:
+        with open(sys.argv[2], 'w') as html:
+            unordered_start, ordered_start, paragraph = False, False, False
+            # bold syntax
+            for line in read:
+                line = line.replace('**', '<b>', 1)
+                line = line.replace('**', '</b>', 1)
+                line = line.replace('__', '<em>', 1)
+                line = line.replace('__', '</em>', 1)
 
-    '''
-    Reads markdown file and parses content
-    '''
+                # md5
+                md5 = re.findall(r'\[\[.+?\]\]', line)
+                md5_inside = re.findall(r'\[\[(.+?)\]\]', line)
+                if md5:
+                    line = line.replace(md5[0], hashlib.md5(
+                        md5_inside[0].encode()).hexdigest())
 
-    with open(markdown_file, 'r') as file:
-        md_parser_list = file.readlines()
-    md_parser_list = ''.join(md_parser_list).split('\n')
+                # remove the letter C
+                remove_letter_c = re.findall(r'\(\(.+?\)\)', line)
+                remove_c_more = re.findall(r'\(\((.+?)\)\)', line)
+                if remove_letter_c:
+                    remove_c_more = ''.join(
+                        c for c in remove_c_more[0] if c not in 'Cc')
+                    line = line.replace(remove_letter_c[0], remove_c_more)
 
-    '''
-    Call header method and add to parsed markdown list
-    '''
-    md_parser_list = parse_headings(md_parser_list)
-    md_parser_list = '\n'.join(md_parser_list).split('\n')
+                length = len(line)
+                headings = line.lstrip('#')
+                heading_num = length - len(headings)
+                unordered = line.lstrip('-')
+                unordered_num = length - len(unordered)
+                ordered = line.lstrip('*')
+                ordered_num = length - len(ordered)
+                # headings, lists
+                if 1 <= heading_num <= 6:
+                    line = '<h{}>'.format(
+                        heading_num) + headings.strip() + '</h{}>\n'.format(
+                        heading_num)
 
-    '''
-    Call ordered list method and add to parsed markdown list
-    '''
-    md_parser_list = parse_ordered_list(md_parser_list)
-    md_parser_list = '\n'.join(md_parser_list).split('\n')
+                if unordered_num:
+                    if not unordered_start:
+                        html.write('<ul>\n')
+                        unordered_start = True
+                    line = '<li>' + unordered.strip() + '</li>\n'
+                if unordered_start and not unordered_num:
+                    html.write('</ul>\n')
+                    unordered_start = False
 
-    '''
-    Call unordered list method and add to parsed markdown list
-    '''
-    md_parser_list = parse_unordered_list(md_parser_list)
-    md_parser_list = '\n'.join(md_parser_list).split('\n')
+                if ordered_num:
+                    if not ordered_start:
+                        html.write('<ol>\n')
+                        ordered_start = True
+                    line = '<li>' + ordered.strip() + '</li>\n'
+                if ordered_start and not ordered_num:
+                    html.write('</ol>\n')
+                    ordered_start = False
 
-    '''
-    Create HTML file and write converted markdown to html tags
-    '''
-    with open(output_file, 'w') as file:
-        for md_symbol in md_parser_list:
-            file.write(md_symbol + '\n')
+                if not (heading_num or unordered_start or ordered_start):
+                    if not paragraph and length > 1:
+                        html.write('<p>\n')
+                        paragraph = True
+                    elif length > 1:
+                        html.write('<br/>\n')
+                    elif paragraph:
+                        html.write('</p>\n')
+                        paragraph = False
 
+                if length > 1:
+                    html.write(line)
 
-if __name__ == "__main__":
-    file_read_write()
+            if unordered_start:
+                html.write('</ul>\n')
+            if ordered_start:
+                html.write('</ol>\n')
+            if paragraph:
+                html.write('</p>\n')
+    exit (0)
