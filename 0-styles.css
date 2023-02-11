df9924b25f154a55c4788b3e5fd54bdd0affb13b
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-02-11 12:08:52 +0300

    Sprite languages

---
+++ 0-styles.css
@@ -0,0 +1,49 @@
+*, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+html, body {
+    height: 100vh;
+    font-weight: bold;
+    font-size: 1.5rem;
+}
+
+ul {
+    display: grid;
+    place-items: center;
+    list-style: none;
+    height: 100vh;
+    width: 100%;
+    grid-template-columns: repeat(3, 1fr);
+}
+
+li {
+    border: 2.5px solid #727272;
+    background-color: #f7f7f7;
+    border-radius: 10px;
+    width: 300px;
+    height: 550px;
+    display: grid;
+    place-items: center;
+    grid-template-rows: 1fr 2.5fr;
+}
+
+span {
+    height:  100%;
+    width: 220px;
+    padding: 50px;
+}
+
+span.i-html {
+    background: url('0-sprite.png') no-repeat -30px -40px;
+}
+
+span.i-css {
+    background: url('0-sprite.png') no-repeat -320px -40px;
+}
+
+span.i-js {
+    background: url('0-sprite.png') no-repeat -610px -40px;
+}
